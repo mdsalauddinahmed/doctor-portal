8a17a6b8bc38a4fa17678907d09a6e2c92f8f0a3
salauddinAmir <mdsalauddinahmed.amir@gmail.com>
2022-11-22 23:19:37 +0600

    business card added

--- src/components/Home/InfoCard/InfoCard.js
+++ src/components/Home/InfoCard/InfoCard.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './info.css'
 
 const InfoCard = ({info}) => {
     return (
-        <div className='col-md-4'>
+        <div className='col-md-4 text-white'>
+            <div className={`d-flex justify-content-center info-container info-${info.background}`}>
+
             <div>
-            <FontAwesomeIcon icon={info.icon}></FontAwesomeIcon>
+            <FontAwesomeIcon className='info-icons' icon={info.icon}></FontAwesomeIcon>
             </div>
             <div>
                 <h6>{info.title}</h6>
                 <small>{info.description}</small>
             </div>
+
+            </div>
+            
             
         </div>
     );
