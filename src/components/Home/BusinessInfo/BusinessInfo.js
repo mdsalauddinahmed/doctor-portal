2f9bf0fa995e3b9092363ded0a9ed9cb3148a6ea
salauddinAmir <mdsalauddinahmed.amir@gmail.com>
2022-11-22 22:49:36 +0600

    business info

--- src/components/Home/BusinessInfo/BusinessInfo.js
+++ src/components/Home/BusinessInfo/BusinessInfo.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import InfoCard from '../InfoCard/InfoCard';
+import { faClock,faMapMarker,faPhone } from '@fortawesome/free-solid-svg-icons'
   const infosdata =[{
     title:'Opening Hours',
     description:'we are open 7 days',
-    icon:'',
+    icon:faClock,
     background:'primary'
   },
    {
     title:'Visit out location',
     description:'Brooklyn, NY 10002 USA',
-    icon:'',
+    icon:faMapMarker,
     background:'dark'
   },
   {
     title:'Call us now',
     description:'+15697854124',
-    icon:'',
+    icon:faPhone,
     background:'primary'
   }
 ]
--- src/components/Home/InfoCard/InfoCard.js
+++ src/components/Home/InfoCard/InfoCard.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const InfoCard = ({info}) => {
     return (
         <div className='col-md-4'>
-            
+            <div>
+            <FontAwesomeIcon icon={info.icon}></FontAwesomeIcon>
+            </div>
+            <div>
+                <h6>{info.title}</h6>
+                <small>{info.description}</small>
+            </div>
             
         </div>
     );
